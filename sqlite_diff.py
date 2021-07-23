009778b307a1673a1f84d08a794d9bafaebe1f27
Samuel Bruce <11494533+samuelbruce@users.noreply.github.com>
2021-07-23 19:09:43 +1000

    Changeset: 5 Omit sqlite_sequence table

--- sqlite_diff.py
+++ sqlite_diff.py
@@ -13,14 +13,22 @@ def exportSql(dbFile):
     connection = sqlite3.connect(dbFile)
     with open(sqlFile, 'w') as f:
         f.write("PRAGMA foreign_keys = off;\n")
+        skipTable = False
         for line in connection.iterdump():
             if "CREATE TABLE" in line:
                 tableName = line[13:line[13:].find(" ")+13]
+                skipTable = False
                 f.write("\n")
                 f.write("--Table: " + tableName + "\n")
+                f.write("%s\n" % line)
+            elif "DELETE FROM" in line:
+				# this is sqlite_sequence table, skip it
+                skipTable = True
+                continue
             elif "COMMIT" in line:
                 continue
-            f.write("%s\n" % line)
+            elif skipTable is False:
+                f.write("%s\n" % line)
         f.write("\n")
         f.write("COMMIT TRANSACTION;\n")
         f.write("PRAGMA foreign_keys = on;\n")
